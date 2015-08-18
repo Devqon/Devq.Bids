ae141442f67ee8bb1c0a5648a34e66858355528c
Devqon <s.geerts@live.nl>
2015-08-18 17:52:56 +0200

    Bid settings

---
+++ Handlers/BidSettingsPartHandler.cs
@@ -0,0 +1,27 @@
+﻿using Devq.Bids.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Localization;
+
+namespace Devq.Bids.Handlers
+{
+    public class BidSettingsPartHandler : ContentHandler {
+
+        public Localizer T { get; set; }
+
+        public BidSettingsPartHandler() {
+            T = NullLocalizer.Instance;
+
+            Filters.Add(new ActivatingFilter<BidSettingsPart>("Site"));
+            Filters.Add(new TemplateFilterForPart<BidSettingsPart>("BidSettings", "Parts.Bids.SiteSettings", "Bids"));
+        }
+
+        protected override void GetItemMetadata(GetContentItemMetadataContext context)
+        {
+            if (context.ContentItem.ContentType != "Site")
+                return;
+            base.GetItemMetadata(context);
+            context.Metadata.EditorGroupInfo.Add(new GroupInfo(T("Bids")));
+        }
+    }
+}
---
+++ Models/BidSettingsPart.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Orchard.ContentManagement;
+
+namespace Devq.Bids.Models
+{
+    public class BidSettingsPart : ContentPart
+    {
+        [Range(0, int.MaxValue, ErrorMessage = "Must be a positive number")]
+        public int ClosedBidsDelay {
+            get { return this.Retrieve(x => x.ClosedBidsDelay); }
+            set { this.Store(r => r.ClosedBidsDelay, value); }
+        }
+    }
+}
